5572f1d6bc10ac9d75ed67af471530b8d437d319
anuradhacodervns <141741902+anuradhacodervns@users.noreply.github.com>
2024-08-25 01:18:49 +0530

    Convert text-to-audio.py

--- Convert text-to-audio.py
+++ Convert text-to-audio.py
@@ -1,25 +1,8 @@
-from flask_cors import CORS  
-import pyttsx3
+from gtts import gTTS
+import os
 
-app = Flask(__name__)
-CORS(app)  
-
-@app.route('/api/text-to-speech', methods=['POST'])
-def text_to_speech():
-    data = request.json
-    text = data.get('text', '')
-    if not text:
-        return jsonify({"error": "No text provided"}), 400
-
-    try:
-        text_speech = pyttsx3.init()
-        text_speech.say(text)
-        text_speech.runAndWait()
-        return jsonify({"message": "Text has been spoken!"}), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+def text_to_audio(text, filename='output.mp3'):
+    tts = gTTS(text)
+    tts.save(filename)
+    os.system(f'mpg321 {filename}')
 
